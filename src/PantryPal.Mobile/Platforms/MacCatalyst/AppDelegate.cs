941df007be8e81c94a9a5a953d61c5eb98bf1037
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2025-10-19 12:45:05 +0200

    feat: project bootstrap

---
+++ src/PantryPal.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PantryPal.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
