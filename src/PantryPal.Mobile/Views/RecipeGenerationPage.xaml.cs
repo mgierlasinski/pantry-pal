8300df32deda122fd26b65921e8b0cee7945bf0b
Marcin Gierlasiński <marcin.gierlasinski@gmail.com>
2025-11-15 17:56:35 +0100

    fix: back navigation on generated recipe

--- src/PantryPal.Mobile/Views/RecipeGenerationPage.xaml.cs
+++ src/PantryPal.Mobile/Views/RecipeGenerationPage.xaml.cs
@@ -19,5 +19,21 @@ public partial class RecipeGenerationPage : ContentPage
             await viewModel.LoadDataCommand.ExecuteAsync(null);
         }
     }
+
+    protected override bool OnBackButtonPressed()
+    {
+        if (BindingContext is RecipeGenerationViewModel viewModel)
+        {
+            // Execute automatic reject with default reason when back button is pressed
+            // This is async but we can't await in a sync method, so we'll fire and forget
+            _ = Task.Run(() => MainThread.InvokeOnMainThreadAsync(async () => await viewModel.RejectCommand.ExecuteAsync(null)));
+
+            // Return true to indicate we've handled the back button press
+            return true;
+        }
+
+        // Fallback to default behavior if no view model
+        return base.OnBackButtonPressed();
+    }
 }
 
